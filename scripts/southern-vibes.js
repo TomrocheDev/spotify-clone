1433fbcbd98c8784ae9c17a0c673f81457db495e
Tom Roche <tomroche.development@gmail.com>
2023-07-18 21:14:44 +0200

    create footer on main section

--- scripts/southern-vibes.js
+++ scripts/southern-vibes.js
@@ -74,5 +74,69 @@ let southernVibes = `
             <div id="suggested-songlist"></div>
         </div>
     </div>
+    <div id="main-footer">
+        <div id="contact-table">
+            <table>
+                <thead>
+                    <th>Bedrijf</th>
+                    <th>Community's</th>
+                    <th>Handige links</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="button">Info</td>
+                        <td class="button">Artiestenservices</td>
+                        <td class="button">Ondersteuning</td>
+                    </tr>
+                    <tr>
+                        <td class="button">Vacatures</td>
+                        <td class="button">Ontwikkelaars</td>
+                        <td class="button">Gratis mobiele app</td>
+                    </tr>
+                    <tr>
+                        <td class="button">For the Record</td>
+                        <td class="button">Reclame</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td class="button">Investeerders</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td class="button">Leveranciers</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td class="button">Spotify for Work</td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div id="socials">
+            <i class="fa-brands fa-instagram social-icon"></i>
+            <i class="fa-brands fa-twitter social-icon"></i>
+            <i class="fa-brands fa-facebook social-icon"></i>
+        </div>
+
+    </div>
+    <hr style="border: none; border-bottom: 1px solid rgba(255, 255, 255, 0.137); margin-top: 2rem" />
+    
+    <div id="bottom-footer">
+        <div id="footer-links">
+            <span class="button">Juridisch</span>
+            <span class="button">Privacycentrum</span>
+            <span class="button">privacybeleid</span>
+            <span class="button">Cookie-instellingen</span>
+            <span class="button">Over advertenties</span>
+            <span class="button">Toegankelijkheid</span>
+        </div>
+        <div id="trademark">
+            <span>© 2023 Spotify AB</span>
+        </div>
+    </div>
 </div>
 `;
